59ef8951986ae63c8c8aa0fddf7725863e6a49e0
Chase Medford <chasemedford1@gmail.com>
2022-11-29 10:54:16 -0600

    Solutions test - minor cleanup.

--- solutions.js
+++ solutions.js
@@ -46,8 +46,8 @@ function addOne(input){
 //5. Is even function. Define a function named isEven that takes in a single input. If the input is an even number
 //or a string containing an even number, return true. Any other input should return false for the output.
 
-function isEven(x){
-    return parseInt(x) % 2 === 0;
+function isEven(input){
+    return parseInt(input) % 2 === 0;
 }
 //6. Define a function named isIdentical that takes in two input arguments. If each input is equal both in data type
 //and in value, then return true. If the values are not the same data type or not the same value, return false.
